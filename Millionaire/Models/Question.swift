b76d2bedebd805d7a3876f610c0052dcd463ac62
Pavel Kuznetsov <59725337+pvlkzntsv@users.noreply.github.com>
2023-02-07 18:00:46 +0700

    Develop (#1)
    * question
    
    * version 12.0 compatible

---
+++ Millionaire/Models/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  Millionaire
+//
+//  Created by pvl kzntsv on 07.02.2023.
+//
+
+import Foundation
---
+++ Millionaire/Models/QuestionBrain.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionBrain.swift
+//  Millionaire
+//
+//  Created by pvl kzntsv on 07.02.2023.
+//
+
+import Foundation
